102f031ed6fcbc60d51c8f11dba4046eaaf03892
kobatakeyann <suu_hppa@outlook.jp>
2024-09-24 21:16:06 +0900

    Add page components

--- resources/ts/components/Home.tsx
+++ resources/ts/components/Home.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 
-function Example() {
+function Home() {
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">Home Component</div>
 
-                        <div className="card-body">
-                            I'm an example component!
-                        </div>
+                        <div className="card-body">I'm an Home component!</div>
                     </div>
                 </div>
             </div>
@@ -18,4 +16,4 @@ function Example() {
     );
 }
 
-export default Example;
+export default Home;

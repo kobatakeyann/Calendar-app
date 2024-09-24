c6486921ee22e66527d3a11e198a881e4ce4fc71
kobatakeyann <suu_hppa@outlook.jp>
2024-09-24 21:12:44 +0900

    Update routing

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+// Route::get('/home', [HomeController::class, 'index']);
+
+// Route::get('/{any}', function () {
+//     return view('app');
+// })->where('any', '.*')->middleware('auth');
+
 
 Route::get('/{any}', function () {
     return view('app');
-})->where('any', '.*')->middleware('auth');
+})->where('any', '.*');

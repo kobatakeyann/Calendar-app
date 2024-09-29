beba699dc2f3e3e3a0de0a14aa1156dfcc6d2639
kobatakeyann <suu_hppa@outlook.jp>
2024-09-29 19:51:49 +0900

    Update homepage structure

---
+++ resources/ts/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Calendar from "../components/calendar/Calendar";
+import Header from "../components/layout/Header";
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <Calendar />
+        </>
+    );
+}
+
+export default Home;

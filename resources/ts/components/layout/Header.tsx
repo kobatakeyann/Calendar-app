eecde694f3fb44bc4e5cfa572f6155cae2fa7f4d
kobatakeyann <suu_hppa@outlook.jp>
2024-09-29 19:54:18 +0900

    Add header component

---
+++ resources/ts/components/layout/Header.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="header-icon-area">
+                <a href="#">
+                    <img
+                        src="/images/app_icon.png"
+                        alt="App Icon"
+                        className="app-icon"
+                    ></img>
+                </a>
+                <h1 className="header-app-title">My Calendar App</h1>
+            </div>
+            <nav>
+                <ul className="header-factor-area">
+                    <li>
+                        <a href="#" className="header-text">
+                            イベント
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="header-text">
+                            お気に入り
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="header-text">
+                            Todo管理
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="header-text">
+                            LINE連携
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}

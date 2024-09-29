50ebf938afdc341a665019bca8d6bbb3c24d2c26
kobatakeyann <suu_hppa@outlook.jp>
2024-09-29 23:41:06 +0900

    Delete example route

--- resources/ts/routes/route.tsx
+++ resources/ts/routes/route.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Example from "../pages/Example";
 import Home from "../pages/Home";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Example />} />
-                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Home />} />
             </Routes>
         </BrowserRouter>
     );

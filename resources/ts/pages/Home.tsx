8ae0fad0439332cc334fb80a094d63b891716581
kobatakeyann <suu_hppa@outlook.jp>
2024-09-29 23:40:31 +0900

    Add sidebar component

--- resources/ts/pages/Home.tsx
+++ resources/ts/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Calendar from "../components/calendar/Calendar";
 import Header from "../components/layout/Header";
+import Sidebar from "../components/layout/Sidebar";
 
 function Home() {
     return (
         <>
             <Header />
+            <Sidebar />
             <Calendar />
         </>
     );

7b10d838e8ac4e9f6815c75fc115ae5d08d9ec5c
kobatakeyann <suu_hppa@outlook.jp>
2024-09-29 19:47:34 +0900

    Add react frontend routing

--- resources/ts/routes/route.tsx
+++ resources/ts/routes/route.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Example from "../components/Example";
-import Home from "../components/Home";
+import Example from "../pages/Example";
+import Home from "../pages/Home";
 
 function App() {
     return (

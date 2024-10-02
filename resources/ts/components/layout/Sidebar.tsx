4c90d4044e1bae782d6d5790f581b9b3daf443b1
kobatakeyann <suu_hppa@outlook.jp>
2024-10-02 21:35:10 +0900

    Add Sidebar component

--- resources/ts/components/layout/Sidebar.tsx
+++ resources/ts/components/layout/Sidebar.tsx
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function Sidebar() {
-    return <div className="header-text">I'm Sidebar component!</div>;
+    return (
+        <aside className="sidebar-area">
+            <div className="header-text">I'm Sidebar component!</div>
+            <h2>Schedule</h2>
+            <ul>
+                <li>新規作成</li>
+                <li>編集</li>
+                <li>削除</li>
+            </ul>
+            <h2>TodoList</h2>
+            <ul>
+                <li>一覧</li>
+                <li>追加</li>
+            </ul>
+            <h2>Settings</h2>
+            <ul>
+                <li>テーマ</li>
+                <li>通知</li>
+                <li>hoge</li>
+            </ul>
+            <h2>Settings</h2>
+            <ul>
+                <li>テーマ</li>
+                <li>通知</li>
+                <li>hoge</li>
+            </ul>
+            <h2>Settings</h2>
+            <ul>
+                <li>テーマ</li>
+                <li>通知</li>
+                <li>hoge</li>
+            </ul>
+        </aside>
+    );
 }

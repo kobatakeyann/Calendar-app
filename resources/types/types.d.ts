68dcb8a0b79a415d24b7c5d799184c95afa0983c
kobatakeyann <suu_hppa@outlook.jp>
2024-10-10 22:53:52 +0900

    Add module.css type definition

--- resources/types/types.d.ts
+++ resources/types/types.d.ts
@@ -2,3 +2,8 @@ declare module "*.png" {
     const url: string;
     export default url;
 }
+
+declare module "*.module.css" {
+    const classes: { [key: string]: string };
+    export default classes;
+}

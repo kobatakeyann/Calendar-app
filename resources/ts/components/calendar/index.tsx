e41e8676ebb322d2bcd19fbbcf41a66f40576862
kobatakeyann <suu_hppa@outlook.jp>
2024-10-20 23:30:10 +0900

    Modify type name

--- resources/ts/components/calendar/index.tsx
+++ resources/ts/components/calendar/index.tsx
@@ -3,35 +3,35 @@ import EventDisplay from "@/ts/components/calendar/components/Event";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
 import FullCalendar from "@fullcalendar/react";
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 
-export interface EventObject {
+export interface Event {
 	title: string;
 	date: string;
 }
 
 export interface EventProps {
-	eventObject: EventObject[];
+	eventObject: Event[];
 }
 
 export default function Calendar() {
-	const events: EventObject[] = [
+	const events: Event[] = [
 		{ title: "地惑BBQ", date: "2024-10-04" },
 		{ title: "履修登録", date: "2024-10-07" },
 		{ title: "おでん", date: "2024-10-22" },
 		{ title: "AtCoder", date: "2024-10-26" },
 		{ title: "Holton", date: "2024-10-18" },
 	];
-	const [selectedDateEvent, setSelectedDateEvent] = useState<EventObject[]>([]);
+	const [selectedDateEvent, setSelectedDateEvent] = useState<Event[]>([]);
 
-	const handleDateClick = useCallback((arg: DateClickArg) => {
+	const handleDateClick = (arg: DateClickArg) => {
 		const targetDateEvent = events.filter(
 			(event) => event.date === arg.dateStr
 		);
 		targetDateEvent
 			? setSelectedDateEvent(targetDateEvent)
 			: setSelectedDateEvent([]);
-	}, []);
+	};
 
 	return (
 		<div className={styles.calendar}>

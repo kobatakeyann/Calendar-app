e397586f639b6313aedc83bca62f830a26beb343
kobatakeyann <suu_hppa@outlook.jp>
2024-10-22 19:40:25 +0900

    Update homepage styles

--- resources/ts/components/calendar/index.tsx
+++ resources/ts/components/calendar/index.tsx
@@ -6,42 +6,43 @@ import FullCalendar from "@fullcalendar/react";
 import React, { useState } from "react";
 
 export interface Event {
-	title: string;
-	date: string;
+  title: string;
+  date: string;
 }
 
 export interface EventProps {
-	eventObject: Event[];
+  eventObject: Event[];
 }
 
 export default function Calendar() {
-	const events: Event[] = [
-		{ title: "地惑BBQ", date: "2024-10-04" },
-		{ title: "履修登録", date: "2024-10-07" },
-		{ title: "おでん", date: "2024-10-22" },
-		{ title: "AtCoder", date: "2024-10-26" },
-		{ title: "Holton", date: "2024-10-18" },
-	];
-	const [selectedDateEvent, setSelectedDateEvent] = useState<Event[]>([]);
+  const events: Event[] = [
+    { title: "地惑BBQ", date: "2024-10-04" },
+    { title: "履修登録", date: "2024-10-07" },
+    { title: "おでん", date: "2024-10-22" },
+    { title: "AtCoder", date: "2024-10-26" },
+    { title: "Holton", date: "2024-10-18" },
+    { title: "せいま colloquium", date: "2024-10-29" },
+  ];
+  const [selectedDateEvent, setSelectedDateEvent] = useState<Event[]>([]);
 
-	const handleDateClick = (arg: DateClickArg) => {
-		const targetDateEvent = events.filter(
-			(event) => event.date === arg.dateStr
-		);
-		targetDateEvent
-			? setSelectedDateEvent(targetDateEvent)
-			: setSelectedDateEvent([]);
-	};
+  const handleDateClick = (arg: DateClickArg) => {
+    const targetDateEvent = events.filter(
+      (event) => event.date === arg.dateStr
+    );
+    targetDateEvent
+      ? setSelectedDateEvent(targetDateEvent)
+      : setSelectedDateEvent([]);
+  };
 
-	return (
-		<div className={styles.calendar}>
-			<FullCalendar
-				plugins={[dayGridPlugin, interactionPlugin]}
-				initialView="dayGridMonth"
-				events={events}
-				dateClick={handleDateClick}
-			/>
-			<EventDisplay eventObject={selectedDateEvent} />
-		</div>
-	);
+  return (
+    <div className={styles.calendar}>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        events={events}
+        dateClick={handleDateClick}
+      />
+      <EventDisplay eventObject={selectedDateEvent} />
+    </div>
+  );
 }

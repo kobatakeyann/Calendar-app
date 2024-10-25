380e650508b93d02b683339c3831b57b355972ad
kobatakeyann <suu_hppa@outlook.jp>
2024-10-25 14:44:32 +0900

    Update registration form

--- resources/ts/components/calendar/components/event_registration_modal/index.tsx
+++ resources/ts/components/calendar/components/event_registration_modal/index.tsx
@@ -16,19 +16,46 @@ export default function EventRegistrationModal(props: ModalProps) {
         id={styles.content}
         onClick={(e) => e.stopPropagation()}
       >
-        <h2>予定を作成</h2>
-        <form>
-          <input
-            type="text"
-            placeholder="タイトルを入力"
-          />
-          <label>日付</label>
-          <input type="date" />
-          <label>説明</label>
-          <textarea></textarea>
+        <h2 className={styles.modalHeader}>予定を作成</h2>
+        <form className={styles.form}>
+          <div className={styles.formElement}>
+            <input
+              type="text"
+              placeholder="タイトルを入力"
+              className={styles.eventInput}
+            ></input>
+          </div>
+          <div className={styles.formElement}>
+            <input
+              type="date"
+              className={styles.dateInput}
+            />
+            <input
+              type="date"
+              className={styles.dateInput}
+            />
+          </div>
+          <div className={styles.formElement}>
+            <textarea
+              placeholder="メモを入力"
+              rows={3}
+              maxLength={50}
+              className={styles.descriptionInput}
+            ></textarea>
+          </div>
         </form>
-        <button type="submit">追加</button>
-        <button onClick={closeRegistrationModal}>閉じる</button>
+        <button
+          type="submit"
+          className={styles.submitButton}
+        >
+          追加
+        </button>
+        <button
+          onClick={closeRegistrationModal}
+          className={styles.closeButton}
+        >
+          ✖
+        </button>
       </div>
     </Fragment>
   );

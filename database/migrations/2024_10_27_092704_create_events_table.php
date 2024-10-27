a1da366bf62942f94a0e10454387052840d0208a
kobatakeyann <suu_hppa@outlook.jp>
2024-10-27 22:15:56 +0900

    Add events table and define relation

---
+++ database/migrations/2024_10_27_092704_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table): void {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->string('title');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->boolean('is_all_day')->default(false);
+            $table->text('description')->nullable();
+            $table->string('location')->nullable();
+            $table->string('color', 7);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};

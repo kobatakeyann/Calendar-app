b64a4d4b31d149825b1508e18725f8c046778eeb
kobatakeyann <suu_hppa@outlook.jp>
2024-10-31 00:29:50 +0900

    Update function of json format

--- resources/ts/services/api/eventService.ts
+++ resources/ts/services/api/eventService.ts
@@ -2,14 +2,16 @@ import { Event } from "@/ts/components/calendar/type";
 
 export const formatEvents = (apiResponse: any[]): Event[] => {
   return apiResponse.map((event) => ({
+    id: event.id,
     title: event.title,
     start: event.start,
     end: event.end,
-    allDay: event.is_all_day,
+    isallday: event.is_allday,
     color: event.color,
     extendedProps: {
-      date: event.start,
+      location: event.location,
       description: event.description,
+      userId: event.user_id,
     },
   }));
 };

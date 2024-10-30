b05b42854fea5e03a20f14f5ae9ef871aa66f216
kobatakeyann <suu_hppa@outlook.jp>
2024-10-31 00:26:53 +0900

    Update api routing

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,8 @@ Route::get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/events', [EventController::class, 'getAllEvents']);
     Route::post('/events', [EventController::class, 'store']);
+    Route::get('/events/on-date', [EventController::class, 'getEventsOnDate']);
     Route::get('/events/{id}', [EventController::class, 'show']);
     Route::put('/events/{id}', [EventController::class, 'update']);
     Route::delete('/events/{id}', [EventController::class, 'destroy']);
-    Route::get('/events/on-date', [EventController::class, 'geteventsOnDate']);
 });

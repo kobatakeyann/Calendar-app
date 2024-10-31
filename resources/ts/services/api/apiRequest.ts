043db5b8249d3129e8802acd5543ae78f9868dfb
kobatakeyann <suu_hppa@outlook.jp>
2024-10-31 19:31:39 +0900

    Add post & put method

--- resources/ts/services/api/apiRequest.ts
+++ resources/ts/services/api/apiRequest.ts
@@ -15,6 +15,34 @@ export async function fetchData<T>(url: string): Promise<T> {
   }
 }
 
+export async function createData<T>(url: string, data: any): Promise<T> {
+  try {
+    const response = await axios.post(url, data, {
+      headers: {
+        "X-XSRF-TOKEN": getXSRFToken(),
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("API request error:", error);
+    throw error;
+  }
+}
+
+export async function updateData<T>(url: string, data: any): Promise<T> {
+  try {
+    const response = await axios.put(url, data, {
+      headers: {
+        "X-XSRF-TOKEN": getXSRFToken(),
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("API request error:", error);
+    throw error;
+  }
+}
+
 export async function deleteData<T>(url: string): Promise<boolean> {
   try {
     await axios.delete(url, {
